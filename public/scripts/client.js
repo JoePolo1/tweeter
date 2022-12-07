b3118f8ac485257b69076999cb1f75a9874f8f69
Joe Polo <joe.polo@telus.com>
2022-12-07 14:18:24 -0800

    fix render function for new tweet data

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -8,27 +8,73 @@
 $(() => {
 
   // Test / driver code (temporary). Eventually will get this from the server.
-  const tweetData = {
-    "user": {
-      "name": "Bilbo Baggins",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@TheRoadGoesEverOn"
-    },
-    "content": {
-      "text": "It's my 111th birthday! 🎂🎂🎂"
-    },
-    "created_at": 1461116232227
-  }
+  // const tweetData = {
+  //   "user": {
+  //     "name": "Bilbo Baggins",
+  //     "avatars": "https://i.imgur.com/73hZDYK.png",
+  //     "handle": "@TheRoadGoesEverOn"
+  //   },
+  //   "content": {
+  //     "text": "It's my 111th birthday! 🎂🎂🎂"
+  //   },
+  //   "created_at": 1461116232227
+  // }
 
-  const $tweet = createTweetElement(tweetData);
+  const data = [
+    {
+      "user": {
+        "name": "Bilbo Baggins",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@TheRoadGoesEverOn"
+      },
+      "content": {
+        "text": "It's my 111th birthday! 🎂🎂🎂"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Frodo Baggins",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@mrUnderhill"
+      },
+      "content": {
+        "text": "Anyone down for a long hike this weekend? I was thinking of heading east, my uncle said they have some great views."
+      },
+      "created_at": 1461113959088
+    }
+  ]
+
+  // const $tweet = createTweetElement(tweetData);
 
   // This appends our new tweet data to tbe bottom of the main tweet container section
-  $('.all-tweets').append($tweet);
+  // $('.all-tweets').append($tweet);
 
+  // TEST - calling renderTweets function here
+  renderTweets(data);
 });
 
-const createTweetElement = function (tweet) {
-  const $tweet = $(`<article class="single-tweet">
+// This function is responsible for rendering and appending new tweets
+const renderTweets = function(tweets) {
+  // Clears pre-existing cached data
+  $('.all-tweets').empty();
+
+  // loops through the array tweets using a For Of loop
+  for (const tweet of tweets) {
+    
+    // calls createTweetElement for each tweet
+    // takes return value and appends it to the tweets container
+    $('.all-tweets').prepend(createTweetElement(tweet));
+  }
+};
+
+
+
+
+const createTweetElement = function(tweet) {
+  const $tweet = $(`
+  <article class="single-tweet">
   <header>
     <!-- Div holds the avatar and name -->
     <div>
