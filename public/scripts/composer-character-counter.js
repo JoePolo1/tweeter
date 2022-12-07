bf9ad05a204637c90877014744e9e8c89962d043
Joe Polo <joe.polo@telus.com>
2022-12-06 16:56:16 -0800

    update article section for new tweet and existing tweet. Include avatar images. stylizing

--- public/scripts/composer-character-counter.js
+++ public/scripts/composer-character-counter.js
@@ -1,3 +1,32 @@
 $(document).ready(function() {
-  console.log("I'm ready! Are you?!")
+  $("#tweet-text").on("input", onInput)
+
 });
+
+// Responsible for adding to the counter of characters after being called above on input
+const onInput = function()  {
+  
+  const input = $(this);
+  const text = input.val();
+  const count = (140 - text.length);
+
+  const output = $(this).siblings(".buttonCounter").children(".counter");
+
+  if (count < 0)  {
+    $(".counter").addClass("counterOverLimit");
+  }
+  if (count > 0)  {
+    $(".counter").removeClass("counterOverLimit");
+  }
+  console.log(count);
+
+  output.text(count);
+}
+
+
+
+// .buttonCounter
+// .counterOverLimit 
+
+
+
