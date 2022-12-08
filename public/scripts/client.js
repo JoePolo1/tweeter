0e305cf02e0670c7547eaa0965ddf9c401157ea2
Joe Polo <joe.polo@telus.com>
2022-12-07 19:01:24 -0800

    add functionality to prevent script attacks through escaper and organization of variables

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,55 +4,44 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// Arrow version of a DOCUMENT READY jquery function
+// Fat arrow version of a DOCUMENT READY jquery function
 $(() => {
 
-  // const data = [
-  //   {
-  //     "user": {
-  //       "name": "Bilbo Baggins",
-  //       "avatars": "https://i.imgur.com/73hZDYK.png"
-  //       ,
-  //       "handle": "@TheRoadGoesEverOn"
-  //     },
-  //     "content": {
-  //       "text": "It's my 111th birthday! 🎂🎂🎂"
-  //     },
-  //     "created_at": 1461116232227
-  //   },
-  //   {
-  //     "user": {
-  //       "name": "Frodo Baggins",
-  //       "avatars": "https://i.imgur.com/nlhLi3I.png",
-  //       "handle": "@mrUnderhill"
-  //     },
-  //     "content": {
-  //       "text": "Anyone down for a long hike this weekend? I was thinking of heading east, my uncle said they have some great views."
-  //     },
-  //     "created_at": 1461113959088
-  //   }
-  // ]
-
-  // renderTweets(data);
+  const escaper = function(string)  {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(string));
+    return div.innerHTML;
+  };
 
   // Responsible for the input form data 
   const $inputForm = $(".tweet-form")
+
   // Responsible for the input form data 
   $inputForm.on("submit", (event) => {
+
     // Prevents page refresh
     event.preventDefault();
+
     // Serializes the input text into URL Encoded data
+    const rawInput = $("#tweet-text").val().trim();
     const formTextInput = $inputForm.serialize();
+    
+    // Runs validator prior to post. False means the condition is a fail and to stop running.
+    if (validator(rawInput) === false)  {
+      return;
+    }
+    
     // Creates POST request to server
     $.post("/tweets", formTextInput, (response) => {
       //TEST log to console. 
       console.log(response);
+
       // Loads the new post onto the page immediately
       tweetFetcher();
     })
   });
 
-  // Responsible for laoding/getting/fetching tweets from server
+  // Responsible for loading/getting/fetching tweets from server
   const tweetFetcher = function() {
     $.get('/tweets', (tweets) =>  {
       console.log(tweets);
@@ -63,6 +52,18 @@ $(() => {
   // Calls tweetFetcher on page load
   tweetFetcher();
 
+
+  // Helper Function for validator
+  // Limits input to only occur when within limitations of a new tweet (length, content etc)
+  const validator = function(tweetInput) {
+    if ((tweetInput === null) || (tweetInput === "")) {
+      alert("Your tweet cannot be empty. Please write a thoughtful message :)");
+      return false;
+    } else if (tweetInput.length > 140) {
+      alert("Your tweet is over 140 characters. Please shorten your message :)");
+      return false;
+    }
+  };
 
   // This function is responsible for rendering and appending new tweets
   const renderTweets = function (tweets) {
@@ -79,38 +80,46 @@ $(() => {
 
   // This function is responsible for configuring the elements of a tweet with the provided format
   const createTweetElement = function (tweet) {
+    const timeFormatted = timeago.format(new Date(tweet.created_at));
+    const fullName = tweet.user.name;
+    const username = tweet.user.handle;
+    const inputText = tweet.content.text;
+    const avatar = tweet.user.avatars;
     const $tweet = $(`
-  <article class="single-tweet">
-  <header>
-    <!-- Div holds the avatar and name -->
-    <div>
-      <img class="tweet-avatar" src=${tweet.user.avatars}>
-      <h4 class="full-name">${tweet.user.name}</h4>
-    </div>
+    <article class="single-tweet">
+    <header>
+      <!-- Div holds the avatar and name -->
+      <div>
+        <img class="tweet-avatar" src=${avatar}>
+        <h4 class="full-name">${fullName}</h4>
+      </div>
 
-    <!-- Username handle is also stored in the header -->
-    <h4 class="user-handle">${tweet.user.handle}</h4>
-  </header>
+      <!-- Username handle is also stored in the header -->
+      <h4 class="user-handle">${username}</h4>
+    </header>
 
-  <!-- Tweet text below -->
-  <div class="tweettext">
-    <p>${tweet.content.text}</p>
-  </div>
-  <footer>
-    <p>${tweet.created_at}</p>
-    <div class="tweet-links">
-      <i class="fa-solid fa-flag"></i>
-      <i class="fa-solid fa-retweet"></i>
-      <i class="fa-solid fa-heart"></i>
+    <!-- Tweet text below -->
+    <div class="tweettext">
+      
+    <p>${escaper(inputText)}</p>
     </div>
-  </footer>
-  </article>
-  <br>`);
+    <footer>
+      <p>${timeFormatted}</p>
+      <div class="tweet-links">
+        <i class="fa-solid fa-flag"></i>
+        <i class="fa-solid fa-retweet"></i>
+        <i class="fa-solid fa-heart"></i>
+      </div>
+    </footer>
+    </article>
+    <br>`);
     console.log(tweet);
     return $tweet;
   };
 
 });
+
+{/* <p>${escaper(inputText)}</p> */}
 
 
 
